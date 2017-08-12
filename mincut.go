3b32c639a2a2732270e291ac1daee492adc99d66
Max Leonard Inden <IndenML@gmail.com>
2017-08-12 13:52:40 +0200

    Add mincut and contraction implementation

--- mincut.go
+++ mincut.go
@@ -1,5 +1,9 @@
 package mincut
 
+import (
+	"math/rand"
+)
+
 type graph struct {
 	n, e  int
 	edges []edge
@@ -14,5 +18,59 @@ type subset struct {
 }
 
 func minCut(g graph) int {
-	return 0
+	subsets := make([]subset, g.n)
+	for i := 0; i < g.n; i = i + 1 {
+		subsets[i].parent = i
+		subsets[i].rank = 0
+	}
+
+	amountNodes := g.n
+
+	rand.Seed(42)
+	for amountNodes > 2 {
+		e := rand.Intn(len(g.edges))
+
+		subsetA := getParent(subsets, g.edges[e].a)
+		subsetB := getParent(subsets, g.edges[e].b)
+
+		if subsetA == subsetB {
+			continue
+		}
+
+		amountNodes = amountNodes - 1
+		contract(subsets, subsetA, subsetB)
+
+	}
+
+	edgesCut := 0
+	for i := 0; i < g.e; i = i + 1 {
+		subsetA := getParent(subsets, g.edges[i].a)
+		subsetB := getParent(subsets, g.edges[i].b)
+		if subsetA != subsetB {
+			edgesCut = edgesCut + 1
+		}
+	}
+
+	return edgesCut
+}
+
+func getParent(subsets []subset, i int) int {
+	if subsets[i].parent != i {
+		subsets[i].parent = getParent(subsets, subsets[i].parent)
+	}
+	return subsets[i].parent
+}
+
+func contract(subsets []subset, subsetA, subsetB int) {
+	parentA := getParent(subsets, subsetA)
+	parentB := getParent(subsets, subsetB)
+
+	if subsets[parentA].rank < subsets[parentB].rank {
+		subsets[parentA].parent = parentB
+	} else if subsets[parentA].rank > subsets[parentB].rank {
+		subsets[parentB].parent = parentA
+	} else {
+		subsets[parentB].parent = parentA
+		subsets[parentA].rank = subsets[parentA].rank + 1
+	}
 }
--- mincut_test.go
+++ mincut_test.go
@@ -27,13 +27,49 @@ var minCutTests = []struct {
 		graph{0, 0, []edge{}},
 		0,
 	},
+	{
+		graph{2, 1, []edge{{0, 1}}},
+		1,
+	},
 }
 
 func TestMinCut(t *testing.T) {
 	for _, test := range minCutTests {
-		cut := minCut(test.g)
+		cut := repeatMinCut(test.g, 1000)
 		if cut != test.cut {
-			t.Fatal(fmt.Sprintf("Expected %v, got %v", test.cut, cut))
+			t.Fatal(fmt.Sprintf("Graph: %v expected %v, got %v", test.g, test.cut, cut))
+		}
+	}
+}
+
+func repeatMinCut(g graph, t int) int {
+	min := minCut(g)
+	for i := 1; i < t; i = i + 1 {
+		newMin := minCut(g)
+		if min > newMin {
+			min = newMin
+		}
+	}
+	return min
+}
+
+var getParentTests = []struct {
+	subsets  []subset
+	i        int
+	expected int
+}{
+	{
+		[]subset{{0, 0}},
+		0,
+		0,
+	},
+}
+
+func TestGetParent(t *testing.T) {
+	for _, test := range getParentTests {
+		out := getParent(test.subsets, test.i)
+		if out != test.expected {
+			t.Fatal(fmt.Sprintf("Expected %v, got %v", test.expected, out))
 		}
 	}
 }
